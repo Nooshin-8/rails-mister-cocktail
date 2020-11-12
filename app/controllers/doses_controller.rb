857b6c212527ea5d087d8a773cf707c2a5ba7a68
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-12 12:17:18 -0500

    made the controller for doses, no view for doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+
+  # A user can add a new dose (ingredient/description pair)
+  # to an existing cocktail, GET "cocktails/42/doses/new"
+
+  # Checkout simple_form docs about f.association to easily create
+  # a select dropdown for our list of ingredients.
+  # POST "cocktails/42/doses"
+
+  def create
+    def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @doses = @cocktail.doses
+      render "cocktails/show"
+    end
+  end
+
+
+    private
+
+    def dose_params
+      params.require(:dose).permit(:description, :ingredient_id)
+    end
+end
