fe1482d1d01aa2ac40420d28c5b1cec1e5692e71
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-13 07:15:14 -0500

    saving everything

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -12,10 +12,11 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktail) # causing the new http request to happen
     else
       @doses = @cocktail.doses
-      render "cocktails/show"
+      render "cocktails/show" # if there are validation errors and is not saved
+      # display all the error
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,13 @@ Rails.application.routes.draw do
   # A user can add a new dose (ingredient/description pair)
   # to an existing cocktail, GET "cocktails/42/doses/new"
 
-  #Checkout simple_form docs about f.association to easily create
+  # Checkout simple_form docs about f.association to easily create
   # a select dropdown for our list of ingredients.
   # POST "cocktails/42/doses"
+
+  # A user can delete a dose that belongs to an existing cocktail.
+  # How can we make a delete link again? DELETE "doses/25"
+  # delete '/cocktails/:id', to: 'cocktails#destroy'
 
   resources :cocktails, only: [:show, :index, :new, :create] do
     resources :doses, only: [:create]
